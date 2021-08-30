7bcb0cf3edc2ee70efff5b0dc25291492dc71302
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-08-30 20:22:40 +0200

    Add roulette and SUS selections (#12)

--- lib/meow_nx/utils.ex
+++ lib/meow_nx/utils.ex
@@ -5,7 +5,21 @@ defmodule MeowNx.Utils do
 
   @doc """
   Given a 2-dimensional tensor swaps each consecutive
-  pair of rows.
+  pairs of rows.
+
+  ## Examples
+
+      iex> t = Nx.iota({4, 2}, axis: 0)
+      iex> MeowNx.Utils.swap_adjacent_rows(t)
+      #Nx.Tensor<
+        s64[4][2]
+        [
+          [1, 1],
+          [0, 0],
+          [3, 3],
+          [2, 2]
+        ]
+      >
   """
   defn swap_adjacent_rows(t) do
     {n, m} = Nx.shape(t)
@@ -20,6 +34,20 @@ defmodule MeowNx.Utils do
   @doc """
   Given a 2-dimensional tensor replicates each row into
   two adjacent rows.
+
+  ## Examples
+
+      iex> t = Nx.tensor([[0, 0], [1, 1]])
+      iex> MeowNx.Utils.duplicate_rows(t)
+      #Nx.Tensor<
+        s64[4][2]
+        [
+          [0, 0],
+          [0, 0],
+          [1, 1],
+          [1, 1]
+        ]
+      >
   """
   defn duplicate_rows(t) do
     {n, m} = Nx.shape(t)
@@ -28,5 +56,29 @@ defmodule MeowNx.Utils do
     t
     |> Nx.tile([1, 2])
     |> Nx.reshape({twice_n, m})
+  end
+
+  @doc """
+  Returns the cumulative sum of elements in the given
+  1-dimensional tensor.
+
+  ## Examples
+
+      iex> MeowNx.Utils.cumulative_sum(Nx.tensor([1, 2, 3, 4]))
+      #Nx.Tensor<
+        s64[4]
+        [1, 3, 6, 10]
+      >
+  """
+  defn cumulative_sum(t) do
+    {n} = Nx.shape(t)
+    Nx.dot(lower_triangular(n), t)
+  end
+
+  defnp lower_triangular(n) do
+    Nx.less_equal(
+      Nx.iota({n, n}, axis: 1),
+      Nx.iota({n, n}, axis: 0)
+    )
   end
 end
---
+++ test/meow_nx/utils_test.exs
@@ -0,0 +1,5 @@
+defmodule MeowNx.UtilsTest do
+  use ExUnit.Case, async: true
+
+  doctest MeowNx.Utils
+end
