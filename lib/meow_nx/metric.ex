f6dd79f074f6cbd965f5844ee5660c8a45099f6d
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-10-16 17:48:28 +0200

    Add diversity metrics (#31)
    * Add fitness entropy
    
    * Add genomes mean Euclidean distance

--- lib/meow_nx/metric.ex
+++ lib/meow_nx/metric.ex
@@ -42,4 +42,51 @@ defmodule MeowNx.Metric do
   defn fitness_sd(_genomes, fitness) do
     MeowNx.Utils.sd(fitness)
   end
+
+  @doc """
+  Calculates fitness entropy.
+
+  Gives a sense of diversity in fitness values. When all
+  values are the same the entropy is equal to 0, the more
+  the values differ, the higher the entropy.
+
+  By default values are grouped by their exact values, but
+  when working in continous space you likely want to treat
+  values as equal if they are close enough. To do that you
+  can specify the `:precision` option.
+
+  ## Options
+
+    * `:precision` - the length of intervals to divide the
+      space into. Values in the same interval will be considered
+      equal. By default exact values are compared
+  """
+  defn fitness_entropy(_genomes, fitness, opts \\ []) do
+    opts = keyword!(opts, [:precision])
+
+    values =
+      transform({fitness, opts[:precision]}, fn
+        {fitness, nil} -> fitness
+        {fitness, precision} -> fitness |> Nx.divide(precision) |> Nx.round()
+      end)
+
+    MeowNx.Utils.entropy(values)
+  end
+
+  @doc """
+  Calculates the mean Euclidean distance between each pair
+  of genomes.
+  """
+  defn genomes_mean_euclidean_distance(genomes, _fitness) do
+    {n, _} = Nx.shape(genomes)
+
+    sum =
+      genomes
+      |> MeowNx.Utils.pairwise_squared_distance()
+      |> Nx.sqrt()
+      |> Nx.sum()
+
+    # Ignore diagonal elements
+    sum / (n * n - n)
+  end
 end
--- lib/meow_nx/utils.ex
+++ lib/meow_nx/utils.ex
@@ -131,6 +131,103 @@ defmodule MeowNx.Utils do
     |> Nx.sqrt()
   end
 
+  @doc """
+  Calculates entropy of values in the given tensor.
+
+  That this function works with exact values. Sometimes different
+  values belong to the same group, in those cases you need to
+  preprocess the tensor beforehand. For example, when working with
+  real numbers, you most likely want to round them, so they are
+  compared with a specific precision.
+
+  Note that this function uses natural logarithm.
+
+  ## Examples
+
+      iex> MeowNx.Utils.entropy(Nx.tensor([1, 1]))
+      #Nx.Tensor<
+        f32
+        -0.0
+      >
+
+      iex> MeowNx.Utils.entropy(Nx.tensor([0.2, 0.22]))
+      #Nx.Tensor<
+        f32
+        0.6931471824645996
+      >
+
+      iex> MeowNx.Utils.entropy(Nx.tensor([2, 1, 3, 1, 2, 1]))
+      #Nx.Tensor<
+        f32
+        1.011404275894165
+      >
+  """
+  defn entropy(t) do
+    {n} = Nx.shape(t)
+
+    sorted = Nx.sort(t)
+
+    # A mask with a single 1 in every group of equal values,
+    # computed by placing 1 where an element differs from its successor
+    representative_mask =
+      Nx.concatenate([
+        Nx.not_equal(sorted[0..-2//1], sorted[1..-1//1]),
+        Nx.tensor([1])
+      ])
+
+    # Calculate frequency for every element
+    prob =
+      Nx.equal(
+        Nx.reshape(sorted, {n, 1}),
+        Nx.reshape(sorted, {1, n})
+      )
+      |> Nx.sum(axes: [0])
+      |> Nx.divide(n)
+
+    prob
+    |> Nx.log()
+    |> Nx.multiply(prob)
+    # Take the sum, but include probability for every unique value just once
+    |> Nx.dot(representative_mask)
+    |> Nx.negate()
+  end
+
+  @doc """
+  Calculates a square of Euclidean distance for every
+  pair of rows in the given 2-dimensional tensor.
+
+  See https://stackoverflow.com/a/37040451
+
+  ## Examples
+
+      iex> t = Nx.tensor([
+      ...>   [0, 0, 0],
+      ...>   [1, 1, 1],
+      ...>   [1, 2, -1]
+      ...> ])
+      iex> MeowNx.Utils.pairwise_squared_distance(t)
+      #Nx.Tensor<
+        s64[3][3]
+        [
+          [0, 3, 6],
+          [3, 0, 5],
+          [6, 5, 0]
+        ]
+      >
+  """
+  defn pairwise_squared_distance(t) do
+    r =
+      t
+      |> Nx.multiply(t)
+      |> Nx.sum(axes: [1])
+      |> Nx.reshape({:auto, 1})
+
+    result = r - 2 * Nx.dot(t, Nx.transpose(t)) + Nx.transpose(r)
+
+    # Make sure there are no negative values due to precision errors
+    Nx.max(result, 0)
+  end
+
   # Macros for use in defn
 
   @doc """
